c6e9bfe8b3167286e39c3a197bb7c63735e6f9d8
uporabnik <uporabnik@164.8.160.77>
2019-05-28 17:50:34 +0200

    Dejansko dodajanje SQL stavkov

---
+++ Praktikum2_Fitness/header.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="registracija.php">Registriraj se</a></li>
+</ul>
+</body>
+</html>
