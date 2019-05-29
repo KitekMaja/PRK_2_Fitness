ec0d4f6a07dea26f8910f359f450180fe8f306cb
Ivana <Ivana@DESKTOP-7L7P21T>
2019-05-29 12:10:18 +0200

    izbrisan template

--- Praktikum2_Fitness/objava.php
+++ Praktikum2_Fitness/objava.php
@@ -1,70 +1,184 @@
 <?php
-// Start the session
-session_start();
-$id_objava = '';
-if (isset($_GET['id'])) {
-    $id_objava = $_GET['id'];
-}
-$_SESSION['idObjava'] = $id_objava;
-?>
+require 'baza.php';
 
+    ?>
 
-<!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="style.css">
-<link rel="stylesheet" type="text/css" href="index.css">
-<title>Objava</title>
-<link rel="stylesheet"
-	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script
-	src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script
-	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet"
-	href="https://fonts.googleapis.com/css?family=Inconsolata">
+<head><title>Objava</title>
+<meta charset="UTF-8"></head>
+
+<body>
 <?php
+// izpis podrobnosti posameznih slik
 
-$servername = "localhost:3306";
-$username = "root";
-$password = "";
-$dbname = "baza";
+$row = FALSE; // predvidevamo, da ni podrobnosti
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$kveri = $sql = "SELECT * FROM objava WHERE objava.id='$id_objava'";
+if (isset($_GET['pid']) && filter_var($_GET['pid'], FILTER_VALIDATE_INT, array('min_range' => 1)) ) { // preverjanje ID slike
+     // preverjanje ID slike
+    
+	$pid = $_GET['pid'];
 
-$result = $conn->query($sql);
+	// pridobivanje podatkov o sliki
+	require ('baza.php');
+	$q = "SELECT * FROM  objava where id=$pid";
+	$r = mysqli_query ($dbc, $q);
+	if (mysqli_num_rows($r) == 1) { // Good to go!
 
-if ($result->num_rows > 0) { 
-    while ($row = $result->fetch_assoc()) {
-        ?>
+		// pridobivanje podatkov
+	    $row = mysqli_fetch_array ($r, MYSQLI_ASSOC);
+	    $rezerva = $row['rezerva'];
 
-	<table style="width: 50%" class="form-horizontal">
+		// zaèetek strani HTML
+		$page_title = $row['naziv'];
 
-	<tr>
-		<th>
-			<h2>
-				<?php echo $row["naziv"] ?>
-				</h2>
-		</th>
-	</tr>
-</table>
+		// prikaz glave
+		echo "<div align=\"center\">
+		<b>{$row['naziv']}</b> ";
 
-<?php
+
+
+		echo "<br />{$row['jezik']}<br/>
+        Rezervirano = $rezerva <br/>";
+		if($rezerva=="NE"){
+        echo "<form  method=\"POST\">
+		<button type=\"submit\" align=\"center\"  name=\"rezerviraj\">Rezerviraj</button></form>
+		</div><br />";
+		}else{echo "</div><br />";}
+
+		// pridobitev podatkov o slik in izpis slike
+		
+		    $db = mysqli_connect("localhost","root","","dsrdb"); //keep your db name
+		    $sql = "SELECT id, slika FROM knjiga WHERE id=$pid";
+		    $sth = $db->query($sql);
+		    $result=mysqli_fetch_array($sth);
+		    echo "<div align=\"center\">";
+		    echo '<img src="data:image/jpeg;base64,'.base64_encode( $result['slika'] ).'"/>';
+		    echo "</div>";
+		    
+	
+	}
+	
+	if (isset($_POST['rezerviraj']) && $row['rezerva'] !== "DA"){
+	   
+	    $host="localhost";
+	    $username='root';
+	    $password='';
+	    $db='dsrdb';
+	    $rez = "DA";
+	    
+	    $connect=mysqli_connect($host,$username,$password, $db) or die("Ni povezave do podatkovne baze");
+	    
+	    $query = "UPDATE knjiga SET rezerva='$rez' WHERE id='$pid'";
+	    
+	    mysqli_query($connect, $query);
+	    //header('Location: '."dashboard.php");
+	}
+	if (isset($_POST['rezerviraj']) && $row['rezerva'] !== "NE"){
+	   
+	    $host="localhost";
+	    $username='root';
+	    $password='';
+	    $db='dsrdb';
+	    $rez = "NE";
+	    
+	    $connect=mysqli_connect($host,$username,$password, $db) or die("Ni povezave do podatkovne baze");
+	    
+	    $query = "UPDATE knjiga SET rezerva='$rez' WHERE id='$pid'";
+	    
+	    mysqli_query($connect, $query);
+	    //header('Location: '."dashboard.php");
+	}
+
+	mysqli_close($dbc);
+
+}else
+if (isset($_GET['ime']) && filter_var($_GET['ime']) ) {
+    
+    $pid = $_GET['ime'];
+   
+    
+    // pridobivanje podatkov o sliki
+    require ('baza.php');
+    $q = "SELECT * FROM  knjiga where naslov='$pid'";
+    $r = mysqli_query ($dbc, $q);
+    if (mysqli_num_rows($r) == 1) { // Good to go!
+        
+        // pridobivanje podatkov
+        $row = mysqli_fetch_array ($r, MYSQLI_ASSOC);
+        
+        // zaèetek strani HTML
+        $page_title = $row['naslov'];
+        $rezerva = $row['rezerva'];
+        
+        
+        // prikaz glave
+        echo "<div align=\"center\">
+		<b>{$row['naslov']}</b> by
+		{$row['avtor']}<br />";
+		
+		
+		echo "<br />{$row['jezik']}<br/>
+        Rezervirano = $rezerva <br/>";
+		if($rezerva=="NE"){
+		    echo "<form  method=\"POST\">
+		<button type=\"submit\" align=\"center\"  name=\"rezerviraj\">Rezerviraj</button></form>
+		</div><br />";
+		}else{echo "</div><br />";}
+		
+		
+		$db = mysqli_connect("localhost","root","","dsrdb"); //keep your db name
+		$sql = "SELECT id, slika FROM knjiga WHERE naslov='$pid'";
+		$sth = $db->query($sql);
+		$result=mysqli_fetch_array($sth);
+		echo "<div align=\"center\">";
+		echo '<img src="data:image/jpeg;base64,'.base64_encode( $result['slika'] ).'"/>';
+		echo "</div>";
+		
     }
-} else {
-    echo "Niii!";
+    
+    if (isset($_POST['rezerviraj']) && $row['rezerva'] !== "DA"){
+      
+        $host="localhost";
+        $username='root';
+        $password='';
+        $db='dsrdb';
+        $rez = "DA";
+        
+        $connect=mysqli_connect($host,$username,$password, $db) or die("Ni povezave do podatkovne baze");
+        
+        $query = "UPDATE knjiga SET rezerva='$rez' WHERE naslov='$pid'";
+        
+        mysqli_query($connect, $query);
+        //header('Location: '."dashboard.php");
+    }
+    if (isset($_POST['rezerviraj']) && $row['rezerva'] !== "NE"){
+      
+        $host="localhost";
+        $username='root';
+        $password='';
+        $db='dsrdb';
+        $rez = "NE";
+        
+        $connect=mysqli_connect($host,$username,$password, $db) or die("Ni povezave do podatkovne baze");
+        
+        $query = "UPDATE knjiga SET rezerva='$rez' WHERE naslov='$pid'";
+        
+        mysqli_query($connect, $query);
+        //header('Location: '."dashboard.php");
+    }
+    
+   
+    
+    mysqli_close($dbc);
+    
 }
-$conn->close();
+    
+
+if (!$row) { // prikaz sporoèila o napakah
+	$page_title = 'Error';
+	echo '<div align="center">This page has been accessed in error!</div>';
+}
+
 ?>
-
-</head>
-
 </body>
 </html>
