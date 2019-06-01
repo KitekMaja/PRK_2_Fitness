da7c764c100cc5258908e2a89228d13d181ef876
Evgen <evgen.tusek@student.um.si>
2019-06-01 19:01:56 +0200

    graf z meritvami in primerom, nova php my admin baza v phpbazaupdate.txt

---
+++ Praktikum2_Fitness/graf.php
@@ -0,0 +1,78 @@
+
+<?php
+//index.php
+
+require 'header.php';
+
+$connect = mysqli_connect("localhost", "root", "", "baza");
+$idu = $_SESSION['id_uporabnika'];
+$queryy = " SELECT * FROM meritve where tk_meritve_uporabnik=$idu";
+$prvameritev;
+$query = " SELECT * FROM meritev where tk_uporabnik=$idu ORDER BY datum asc";
+$dataPointss = array();
+$rr = mysqli_query($connect, $queryy);
+$rrow =  mysqli_fetch_array ($rr, MYSQLI_ASSOC);
+$prvameritev = $rrow['teza'];
+$int = 0;
+$r = mysqli_query ($connect, $query);
+$aray = array();
+$dataa = array("y" => $rrow['teza'], "label" => $rrow['datumVnosa']);
+array_push($dataPointss, $dataa);
+while ($row = mysqli_fetch_array ($r, MYSQLI_ASSOC)) {
+    
+    $aray[$int] =  $row['teza'];
+    $data = array("y" => $row['teza'], "label" => $row['datum']);
+    
+    array_push($dataPointss, $data);
+    $int++;
+}
+$max =0;
+$min = 200;
+foreach($aray as $value) {
+    if($min>$value);
+    $min = $value;
+    
+    if($max<$value)
+        $max=$value;
+}
+if($min >$prvameritev)
+    $min = $prvameritev;
+if($max < $prvameritev)
+    $max = $prvameritev;
+
+echo '</table>';
+mysqli_close($connect);
+
+
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<script>
+window.onload = function () {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	title: {
+		text: "datum"
+	},
+	axisY: {
+
+		minimum: <?php echo json_encode($min); ?>,
+				maximum: <?php echo json_encode($max); ?>,
+		title: "kilogrami"
+	},
+	data: [{
+		type: "line",
+		dataPoints: <?php echo json_encode($dataPointss, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>               
