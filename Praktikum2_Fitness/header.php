f9a31ec1d07be9906237baeacc6518a9df6f2e87
Ivana <Ivana@DESKTOP-7L7P21T>
2019-06-07 19:45:34 +0200

    novi logo, brisanje nepomembnih stvari

--- Praktikum2_Fitness/header.php
+++ Praktikum2_Fitness/header.php
@@ -62,7 +62,7 @@ session_start();
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light ">
   <a class="navbar-brand" href="#"> 
-  	<img src="slike/fitt.png" alt="logo" style="width: 200px;">
+  	<img src="slike/header.png" alt="logo" style="width: 91px;">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
