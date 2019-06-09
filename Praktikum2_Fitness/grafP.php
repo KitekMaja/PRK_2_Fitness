2b5889ecfebac498af1307bcda8812765e6384df
Ivana <Ivana@DESKTOP-7L7P21T>
2019-06-09 20:04:26 +0200

    meni

--- Praktikum2_Fitness/grafP.php
+++ Praktikum2_Fitness/grafP.php
@@ -1,6 +1,6 @@
 
 <?php
-require 'navbar.php';
+require 'header.php';
 include 'PHP_skripte/baza_handler.php';
 include 'PHP_skripte/baza_OOPhandler.php';
 include 'sloutf.php';
@@ -27,75 +27,69 @@ if (mysqli_num_rows($r) == 1) { // Good to go!
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Izpis</title>
-<link
-	href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css"
-	rel="stylesheet" id="bootstrap-css">
+<meta charset="utf-8">
+<!--  This file has been downloaded from https://bootdey.com  -->
+<!--  All snippets are MIT license https://bootdey.com/license -->
+<title>Graf</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <script
-	src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<link
+	href="http://netdna.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
+	rel="stylesheet">
+
 <link rel="stylesheet" href="MojCSS/profile.css">
 
-<link rel="stylesheet" href="MojCSS/izpis.css">
-<!------ Include the above in your HEAD tag ---------->
 </head>
 <body>
 
+	<link
+		href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"
+		rel="stylesheet">
 	<div class="container">
-		<div class="row profile">
-			<div class="col-md-3">
-				<div class="profile-sidebar">
-					<!-- SIDEBAR USERPIC -->
-					<div class="profile-userpic">
-						 <?php  echo "<p><br/>{$slika}</p>"; ?>
+
+		<div class="row">
+			<div class="col-md-4 mb30">
+				<div class="card">
+
+					<div class="card-content pt20 pb20 profile-header">
+                     <?php  echo "<p><br/>{$slika}</p>"; ?>
+                    <h4 class="card-title text-center mb20"> <?php  echo "<p><br/>{$ime}  {$priimek}</p>"; ?><?php  echo "<p>{$email}</p>"; ?> </h4>
+
+						<hr>
+
+						<div class="profile-userbuttons">
+
+							<a href="profil.php" class="btn btn-light btn-block">Profil</a>
+							<br> <a href="meritvePrikaz.php"
+								class="btn btn-light btn-block">Senami</a> <br> <a
+								href="meritvePrikaz.php" class="btn btn-light btn-block">Graf</a>
+							<br> <a href="meritvePrikaz.php"
+								class="btn btn-light btn-block">Meritve</a> <br> <br>
+						</div>
 					</div>
-					<!-- END SIDEBAR USERPIC -->
-					<!-- SIDEBAR USER TITLE -->
-					<div class="profile-usertitle">
-					 <?php  echo "<p><br/>{$ime}</p>"; ?>
-						<div class="profile-usertitle-name"></div>
-						<?php  echo "<p><br/>{$priimek}</p>";  ?>
-						<div class="profile-usertitle-job"><?php  echo "<p><br/>{$email}</p>";?></div>
-					</div>
-					<!-- END SIDEBAR USER TITLE -->
-					<!-- SIDEBAR BUTTONS -->
-					<div class="profile-userbuttons">
-						<a href="dodajnovomeritev.php" class="btn btn-info">Meritve</a>
+					<!--content-->
 
-					</div>
-
-					<!-- END SIDEBAR BUTTONS -->
-					<!-- SIDEBAR MENU -->
-					<div class="profile-usermenu">
-						<ul class="nav">
-							<li ><a href="#"> <i
-									class="glyphicon glyphicon-home"></i> Profil
-							</a></li>
-							<li><a href="#"> <i class="glyphicon glyphicon-user"></i> Uredi
-							</a></li>
-							<li class="active"><a href="graf.php"> <i class="glyphicon glyphicon-stats"></i>
-									Graf
-							</a></li>
-							<li><a href="dodajnovomeritev.php"> <i
-									class="glyphicon glyphicon-heart-empty"></i> Vaje
-							</a></li>
-							<li><a href="dodajnovomeritev.php"> <i
-									class="glyphicon glyphicon-list-alt"></i> Objave
-							</a></li>
-
-
-						</ul>
-
-
-						</ul>
-					</div>
-					<!-- END MENU -->
 				</div>
 			</div>
-			<div class="col-md-9">
-				
+			<div class="col-md-8 mb30">
+				<div class="card">
+					<div>
+
+						<!-- Nav tabs -->
+						<ul class="nav tabs-admin" role="tablist">
+							<li role="presentation" class="nav-item"><a
+								class="nav-link active" href="#t1" aria-controls="t1" role="tab"
+								data-toggle="tab">Graf</a></li>
+						</ul>
+
+						<!-- Tab panes -->
+						<div class="tab-content admin-tab-content pt30">
+							<div role="tabpanel" class="tab-pane active show" id="t1">
+							
+											
 <?php
 $connect = mysqli_connect("localhost", "root", "", "baza");
 $idu = $_SESSION['id_uporabnika'];
@@ -172,12 +166,22 @@ chart.render();
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 
 			</div>
+							
+							
+							<div role="tabpanel" class="tab-pane" id="t4"></div>
+						</div>
+
+					</div>
+				</div>
+			</div>
 		</div>
+	</div>
+
+	<script
+		src="http://netdna.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
+	<script type="text/javascript">
 	
 
-	<br>
-	<br>
-
-	<br>
+</script>
 </body>
 </html>
